d01fbf58e61151c63bbc6d49ad69c07fbc47cd24
nd <nd@hypr.sh>
2024-06-11 01:45:11 +0200

    Add keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -33,8 +33,14 @@ config.font = wezterm.font("CaskaydiaCove Nerd Font")
 config.font_size = 17.0
 config.line_height = 1.4
 
--- window
-config.enable_tab_bar = false
+-- tabbar
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- styling
+config.inactive_pane_hsb = { brightness = 0.9 }
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 20
 config.native_macos_fullscreen_mode = true
@@ -47,5 +53,33 @@ config.window_padding = {
 }
 config.window_close_confirmation = "NeverPrompt"
 config.enable_scroll_bar = false
+
+-- keys
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1003 }
+config.keys = {
+	{ key = "\\", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+	{ key = "w", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Down", 6 }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "LeftArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{ key = "a", mods = "LEADER", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
+}
+for i = 1, 8 do
+	-- LEADER + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
 
 return config
