47c1ce7caaabde22fb2ef94fc4bb761efffbdb17
nd <nd@hypr.sh>
2024-06-11 00:09:59 +0200

    removed unused tools

--- nvim/init.lua
+++ nvim/init.lua
@@ -437,8 +437,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- elixirls = {},
-        lexical = {},
+        elixirls = {},
+        -- lexical = {},
         html = {},
         gopls = {},
         cssls = {},
