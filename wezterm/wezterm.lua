2ddecd6c93d6c4ea3053887dbcaf6097481c7985
nd <nd@hypr.sh>
2024-07-30 20:18:42 +0200

    Change font to monolisa and adjust keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -29,9 +29,8 @@ config.adjust_window_size_when_changing_font_size = false
 config.colors = scheme_for_appearance(get_appearance())
 
 -- font
--- config.font = wezterm.font("FiraCode Nerd Font")
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
-config.font_size = 17.0
+config.font = wezterm.font_with_fallback({ "MonoLisa", "JetBrains Mono" })
+config.font_size = 15.0
 config.line_height = 1.4
 
 -- tabbar
@@ -65,11 +64,11 @@ config.keys = {
 	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
 	{ key = "f", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "w", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 	{ key = "h", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "j", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 6 }) },
+	{ key = "j", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
 	{ key = "k", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{ key = "l", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
 	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
