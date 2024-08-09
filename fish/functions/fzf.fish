e63a5668d4b72932b725777685d7c460238d7b49
nd <nd@hypr.sh>
2024-08-09 07:26:06 +0200

    add fzf config

---
+++ fish/functions/fzf.fish
@@ -0,0 +1,26 @@
+function fzf --wraps="fzf"
+    # rosepine dawn
+    # set -Ux FZF_DEFAULT_OPTS "
+    #   --color=fg:#797593,bg:#faf4ed,hl:#d7827e
+    #   --color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e
+    #	--color=border:#dfdad9,header:#286983,gutter:#faf4ed
+    #	--color=spinner:#ea9d34,info:#56949f
+    #	--color=pointer:#907aa9,marker:#b4637a,prompt:#797593"
+
+    # rosepine moon
+    # set -Ux FZF_DEFAULT_OPTS "
+    #   --color=fg:#908caa,bg:#232136,hl:#ea9a97
+    #   --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+    #   --color=border:#44415a,header:#3e8fb0,gutter:#232136
+    #   --color=spinner:#f6c177,info:#9ccfd8
+    #   --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
+    # rosepine
+    set -Ux FZF_DEFAULT_OPTS "
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+    command fzf
+end
