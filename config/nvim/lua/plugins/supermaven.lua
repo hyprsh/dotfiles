c225ea8fe9b224d30a7d98ca514e46b13831c8da
Your Name <you@example.com>
2024-09-20 11:39:53 +0200

    fix: supermaven config

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -2,24 +2,10 @@ return {
   'supermaven-inc/supermaven-nvim',
   config = function()
     require('supermaven-nvim').setup {
-      -- Enable automatic imports
-      auto_import = true,
-      -- Set up code completion
-      completion = {
-        enable = true,
-        trigger_characters = { '.', ':' },
-      },
-      -- Configure diagnostics
-      diagnostics = {
-        enable = true,
-        virtual_text = true,
-        signs = true,
-        underline = true,
-      },
-      -- Set up formatting
-      formatting = {
-        enable = true,
-        format_on_save = true,
+      keymaps = {
+        accept_suggestion = '<Tab>',
+        clear_suggestion = '<C-]>',
+        accept_word = '<C-j>',
       },
     }
   end,
