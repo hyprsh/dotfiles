17dae786bccabf90e010895a540722ef93a9292e
Your Name <you@example.com>
2024-09-20 11:54:26 +0200

    fix: keybinds for aider

--- config/nvim/lua/plugins/aider.lua
+++ config/nvim/lua/plugins/aider.lua
@@ -2,8 +2,10 @@ return {
   'joshuavial/aider.nvim',
   config = function()
     require('aider').setup {
-      -- You can add any configuration options here
-      -- For now, we'll use the default settings
+      -- -- set a keybinding for the AiderOpen function
+      vim.api.nvim_set_keymap('n', '<leader>oa', '<cmd>lua AiderOpen()<cr>', { noremap = true, silent = true }),
+      -- set a keybinding for the AiderBackground function
+      vim.api.nvim_set_keymap('n', '<leader>ob', '<cmd>lua AiderBackground()<cr>', { noremap = true, silent = true }),
     }
   end,
 }
