6b2c3012ac454c1fe0257e7cd92cfd70c9edfd7a
Your Name <you@example.com>
2024-10-02 17:08:02 +0200

    feat: colorvoid theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm")
 local act = wezterm.action
 
 local config = wezterm.config_builder()
@@ -26,15 +25,24 @@ config.scrollback_lines = 5000
 config.adjust_window_size_when_changing_font_size = false
 
 -- set color_scheme
-config.colors = scheme_for_appearance(get_appearance())
+-- config.colors = scheme_for_appearance(get_appearance())
+config.colors = {
+	foreground = "#c0c0c0", -- Light gray for text
+	background = "#1c1c1c", -- Dark background
+	cursor_bg = "#1bfd9c", -- Glow for the cursor
+	cursor_border = "#1bfd9c",
+	cursor_fg = "#1c1c1c", -- Cursor text
+
+	selection_bg = "#585858",
+	selection_fg = "#c0c0c0",
+
+	ansi = { "#1c1c1c", "#dea6a0", "#baffc9", "#d6efd8", "#81A1C1", "#1bfd9c", "#7fa1c3", "#c0c0c0" },
+	brights = { "#4C566A", "#BF616A", "#A3BE8C", "#EBCB8B", "#81A1C1", "#B48EAD", "#8FBCBB", "#ECEFF4" },
+}
 
 -- font
-config.font = wezterm.font_with_fallback({
-	"MonoLisa Nerd Font",
-	"MonoLisa",
-	"JetBrains Mono",
-})
-config.font_size = 16.0
+config.font = wezterm.font_with_fallback({ "Iosevka Nerd Font" })
+config.font_size = 18.0
 config.line_height = 1.5
 
 -- tabbar
@@ -47,13 +55,13 @@ config.hide_tab_bar_if_only_one_tab = true
 -- styling
 config.inactive_pane_hsb = { brightness = 0.95 }
 config.window_background_opacity = 0.95
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 0
 config.native_macos_fullscreen_mode = true
--- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_decorations = "RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+-- config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = "1px",
-	right = "1px",
+	left = "8px",
+	right = "8px",
 	bottom = "1px",
 }
 config.window_close_confirmation = "NeverPrompt"
@@ -62,25 +70,25 @@ config.enable_scroll_bar = false
 -- keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1003 }
 config.keys = {
-	{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
-	{ key = "f", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "h", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "j", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
-	{ key = "k", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{ key = "l", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
-	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "\\",         mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "|",          mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "%",          mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-",          mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = '"',          mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "c",          mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "q",          mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
+	{ key = "f",          mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "z",          mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "h",          mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "j",          mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "k",          mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "l",          mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "LeftArrow",  mods = "LEADER", action = act.ActivatePaneDirection("Left") },
 	{ key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "UpArrow", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "DownArrow", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "UpArrow",    mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "DownArrow",  mods = "LEADER", action = act.ActivatePaneDirection("Down") },
 	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-	{ key = "a", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "a",          mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
 	{
 		key = ",",
 		mods = "LEADER",
