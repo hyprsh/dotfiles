a5ab03c7940737f2aa17dd99e9c0d6b332155c96
Your Name <you@example.com>
2024-10-10 14:33:24 +0200

    feat: remove hyperkey

--- scripts/gather-settings-darwin.sh
+++ scripts/gather-settings-darwin.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-# copy settings files
-cp $HOME/Library/Preferences/com.knollsoft.Hyperkey.plist config/hyperkey/com.knollsoft.Hyperkey.plist
-
 # generate bundlefile
 brew bundle dump --global --force
 cp $HOME/.Brewfile config/brew/Brewfile
