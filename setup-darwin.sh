9374a19f8276573d5b6afcd97f7890c65f60f4a3
Your Name <you@example.com>
2024-11-11 22:54:09 +0100

    cleanup

--- setup-darwin.sh
+++ setup-darwin.sh
@@ -8,14 +8,13 @@ DST="$HOME/.config"
 rm -r $HOME/iCloud
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" "$HOME/iCloud"
 
-rm -f $HOME/.bashrc && ln -s $SRC/bash/bashrc $HOME/.bashrc
-rm -f $HOME/.inputrc && ln -s $SRC/bash/inputrc $HOME/.inputrc
-rm -f $HOME/.zshrc && ln -s $SRC/zsh/zshrc $HOME/.zshrc # TODO:  migrate to bash
+# rm -f $HOME/.bashrc && ln -s $SRC/bash/bashrc $HOME/.bashrc
+# rm -f $HOME/.inputrc && ln -s $SRC/bash/inputrc $HOME/.inputrc
+rm -f $HOME/.zshrc && ln -s $SRC/zsh/zshrc $HOME/.zshrc
 ln -s $SRC/tmux $DST/tmux
 ln -s $SRC/wezterm $DST/wezterm
 ln -s $SRC/git $DST/git
 ln -s $SRC/nvim $DST/nvim
-ln -s $SRC/skhd $DST/skhd
 
 # install brew
 if type brew &>/dev/null; then
@@ -24,10 +23,10 @@ fi
 
 # cli tools
 brew install bat btop eza fd fzf gh neovim procs ripgrep tealdeer \
-    tmux trash-cli yq jq zoxide lazygit ffmpeg skhd zf mas
+    tmux trash-cli yq jq zoxide lazygit ffmpeg mas
 
 # apps
-brew install --cask betterdisplay wezterm discord zen-browser steam
+brew install --cask betterdisplay wezterm discord steam
 
 # appstore apps
 mas install 1352778147 # bitwarden
