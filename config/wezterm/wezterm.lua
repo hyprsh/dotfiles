bc7e945dd56e9d0f47c9c23b6a48c506a4ac3c11
Your Name <you@example.com>
2024-11-17 07:43:00 +0100

    housekeeping

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 
+-- helpers
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
 end
@@ -13,8 +13,8 @@ local is_dark = function()
 	return wezterm.gui.get_appearance():find("Dark") ~= nil
 end
 
-local config = wezterm.config_builder()
 -- settings
+local config = wezterm.config_builder()
 config.default_prog = is_darwin() and { "/opt/homebrew/bin/tmux" } or { "toolbox", "run", "tmux" }
 config.use_dead_keys = false
 config.scrollback_lines = 5000
@@ -34,65 +34,13 @@ config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
 -- styling
-config.inactive_pane_hsb = { brightness = 0.95 }
+-- config.inactive_pane_hsb = { brightness = 0.95 }
 -- config.window_background_opacity = 0.95
 -- config.macos_window_background_blur = 20
 config.window_decorations = is_linux() and "TITLE | RESIZE" or "INTEGRATED_BUTTONS|RESIZE"
 config.enable_wayland = is_linux() and false -- https://github.com/wez/wezterm/issues/4962
-config.window_padding = {
-	left = "16px",
-	right = "16px",
-	top = is_linux() and "8px" or "48px",
-	bottom = "1px",
-}
+config.window_padding = { left = "16px", right = "16px", top = is_linux() and "8px" or "48px", bottom = "1px" }
 config.window_close_confirmation = "NeverPrompt"
 config.enable_scroll_bar = false
-
--- keys
-config.leader = { key = "\\", mods = "CTRL", timeout_milliseconds = 1003 }
-config.keys = {
-	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
-	{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
-	{ key = "f", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "h", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "j", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
-	{ key = "k", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{ key = "l", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
-	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "UpArrow", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "DownArrow", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-	{ key = "a", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
-	{
-		key = ",",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-}
-for i = 1, 8 do
-	-- LEADER + number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i - 1),
-	})
-end
 
 return config
