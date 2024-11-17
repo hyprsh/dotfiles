56e9fd1e17fbcf5e43d3ebe9ac9ef8de53d53814
Your Name <you@example.com>
2024-11-17 05:23:55 +0100

    add textobjects to treesitter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -303,6 +303,7 @@ require('lazy').setup({
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     config = function()
       local configs = require('nvim-treesitter.configs')
 
@@ -311,6 +312,24 @@ require('lazy').setup({
         highlight = { enable = true },
         indent = { enable = true },
         autotag = { enable = true, enable_close_on_slash = false },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
+          },
+        },
+        textobjects = {
+          move = {
+            enable = true,
+            goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+            goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+            goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+            goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+          },
+        },
       })
     end,
   },
